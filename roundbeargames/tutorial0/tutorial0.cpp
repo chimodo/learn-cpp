0abec692fde3e347bad8e0af65f9e1476d4c1dd3
= <=>
2025-05-13 10:46:55 +0200

    tutorial 0.1

---
+++ roundbeargames/tutorial0/tutorial0.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!\n";
+    std::cout << "This works too" << std::endl;
+    std::cout << R"(
+    this
+    is
+    a
+    raw string!
+    )" << std::endl;
+}
