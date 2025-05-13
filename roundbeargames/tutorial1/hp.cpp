661c6ef48dfb2e46ad4b48375c6e76da6261f0bf
= <=>
2025-05-13 14:37:00 +0200

    hp

---
+++ roundbeargames/tutorial1/hp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int hp = 5;
+    bool player_dead = true;
+    float xPosition = 1.5;
+    std::cout << "Player HP is " << hp << std::endl;
+
+    hp --;
+
+    std::cout << "Player HP is now " << hp << std::endl;
+    std::cout << "Is player Dead? " << player_dead << std::endl;
+    std::cout << "Player position? " << player_dead << std::endl;
+
+    // std::string str;
+
+    // std::cin >> str;
+}
